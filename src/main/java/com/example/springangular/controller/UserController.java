e2819a2869961c9fe47ed504268ef7d1b264fda6
jfsppsDev <jfsppsDev@gmail.com>
2021-06-25 23:46:20 +0100

    added register endpoint

--- src/main/java/com/example/springangular/controller/UserController.java
+++ src/main/java/com/example/springangular/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.example.springangular.controller;
 
+import com.example.springangular.domain.security.User;
 import com.example.springangular.exception.ExceptionHandling;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springangular.exception.domain.EmailAlreadyExistException;
+import com.example.springangular.exception.domain.UserNotFoundException;
+import com.example.springangular.exception.domain.UsernameAlreadyExistException;
+import com.example.springangular.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserController extends ExceptionHandling {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/getMessage")
     public String confirmMsg() {
@@ -19,4 +30,15 @@ public class UserController extends ExceptionHandling {
 //    public String getEmails() throws EmailNotFoundException {
 //        throw new EmailNotFoundException("Whoops");
 //    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user)
+            throws UserNotFoundException, EmailAlreadyExistException, UsernameAlreadyExistException {
+        User sentUser = userService.register(user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getEmail());
+
+        return new ResponseEntity<>(sentUser, HttpStatus.OK);
+    }
 }
