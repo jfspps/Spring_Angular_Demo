9990345a29fc60b70d34e97a0a8c92a5b2d7392e
jfsppsDev <jfsppsDev@gmail.com>
2021-06-26 21:28:48 +0100

    correction to AuthenticationSuccessListener

--- src/main/java/com/example/springangular/listener/AuthenticationSuccessListener.java
+++ src/main/java/com/example/springangular/listener/AuthenticationSuccessListener.java
@@ -1,6 +1,6 @@
 package com.example.springangular.listener;
 
-import com.example.springangular.domain.security.User;
+import com.example.springangular.domain.security.UserPrincipal;
 import com.example.springangular.service.LoginAttemptService;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
@@ -16,12 +16,12 @@ public class AuthenticationSuccessListener {
 
     @EventListener
     public void onAuthenticationSuccess(AuthenticationSuccessEvent event){
-        // this should be a User object
+        // this should be a UserPrincipal object
         Object principal = event.getAuthentication().getPrincipal();
 
-        if (principal instanceof User){
-            User user = (User) event.getAuthentication().getPrincipal();
-            loginAttemptService.releaseUserFromCache(user.getUsername());
+        if (principal instanceof UserPrincipal){
+            UserPrincipal userPrincipal = (UserPrincipal) event.getAuthentication().getPrincipal();
+            loginAttemptService.releaseUserFromCache(userPrincipal.getUsername());
         }
     }
 }
