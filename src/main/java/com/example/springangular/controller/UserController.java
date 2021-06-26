dcd07b59bf778871738bf7b2da47997129fa9cc9
jfsppsDev <jfsppsDev@gmail.com>
2021-06-26 14:44:59 +0100

    added login endpoint, with JWT

--- src/main/java/com/example/springangular/controller/UserController.java
+++ src/main/java/com/example/springangular/controller/UserController.java
@@ -1,23 +1,34 @@
 package com.example.springangular.controller;
 
 import com.example.springangular.domain.security.User;
+import com.example.springangular.domain.security.UserPrincipal;
 import com.example.springangular.exception.ExceptionHandling;
 import com.example.springangular.exception.domain.EmailAlreadyExistException;
 import com.example.springangular.exception.domain.UserNotFoundException;
 import com.example.springangular.exception.domain.UsernameAlreadyExistException;
+import com.example.springangular.jwt.JWTTokenProvider;
 import com.example.springangular.service.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static com.example.springangular.constants.SecurityConstant.JWT_TOKEN_HEADER;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserController extends ExceptionHandling {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTTokenProvider jwtTokenProvider;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @GetMapping("/getMessage")
@@ -40,5 +51,28 @@ public class UserController extends ExceptionHandling {
                 user.getEmail());
 
         return new ResponseEntity<>(sentUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+
+        User loggedInUser = userService.findByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loggedInUser);
+
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+
+        return new ResponseEntity<>(loggedInUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+        return httpHeaders;
+    }
+
+    private void authenticate(String username, String password) {
+        // throws and exception if auth fails
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 }
