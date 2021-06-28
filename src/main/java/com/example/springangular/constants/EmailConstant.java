6b36b130895fc4cbad4612a028f195eacde6a7d0
jfspps <jfsppsDev@gmail.com>
2021-06-28 17:29:37 +0100

    added EmailConstant

---
+++ src/main/java/com/example/springangular/constants/EmailConstant.java
@@ -0,0 +1,18 @@
+package com.example.springangular.constants;
+
+public class EmailConstant {
+
+    public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
+    public static final String USERNAME = "yourEmailAddress@gmail.com";
+    public static final String PASSWORD = "passwordHere";
+    public static final String FROM_EMAIL = "support@someCompany.com";
+    public static final String CC_EMAIL = "";
+    public static final String EMAIL_SUBJECT = "Some Company Inc. - new password";
+    public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
+    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMPT_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQURED = "mail.smtp.starttls.required";
+}
