65bb27604d2e53d4bb955165859db387ce929cea
jfspps <jfsppsDev@gmail.com>
2021-06-28 17:46:11 +0100

    built new email message

--- src/main/java/com/example/springangular/service/EmailService.java
+++ src/main/java/com/example/springangular/service/EmailService.java
@@ -2,13 +2,32 @@ package com.example.springangular.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.Properties;
 
 import static com.example.springangular.constants.EmailConstant.*;
 
 @Service
 public class EmailService {
+
+    private Message createEmail(String firstName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello " + firstName + ",\n\nYour new account password is: " + password +
+                "\n\n The support team");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
 
     private Session getEmailSession(){
         Properties properties = System.getProperties();
