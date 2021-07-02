57450a4905e114ff360d6df1997b6e7b42b307d4
jfspps <jfsppsDev@gmail.com>
2021-07-02 12:38:15 +0100

    impl getProfileImage and getTempProfileImage

--- src/main/java/com/example/springangular/controller/UserController.java
+++ src/main/java/com/example/springangular/controller/UserController.java
@@ -23,10 +23,17 @@ import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.websocket.server.PathParam;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import static com.example.springangular.constants.FileConstant.*;
 import static com.example.springangular.constants.SecurityConstant.JWT_TOKEN_HEADER;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @RequestMapping(path = {"/", "/user"})
@@ -148,6 +155,28 @@ public class UserController extends ExceptionHandling {
 
         User user = userService.updateProfileImage(username, profileImage);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/image/{username}/{filename}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getProfileImage(
+            @PathVariable("username") String username, @PathVariable("filename") String filename) throws IOException {
+        return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + filename));
+    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        try(InputStream inputStream = url.openStream()){
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while((bytesRead = inputStream.read(chunk)) > 0){
+                stream.write(chunk, 0, bytesRead);
+            }
+        }
+        return stream.toByteArray();
     }
 
     private ResponseEntity<HttpResponse> response(HttpStatus status, String message) {
